4b2aa24f4d8653df6f507598aecb04942e64ba30
Gabriel Prates <gabsprates@gmail.com>
2020-06-05 21:38:24 -0300

    feat: ajust for deploy

--- core/constants.js
+++ core/constants.js
@@ -3,7 +3,7 @@ const path = require("path");
 const dist = path.resolve(__dirname, "../dist");
 const posts = path.resolve(__dirname, "../posts");
 const assets = path.resolve(__dirname, "../assets");
-const public = path.resolve(__dirname, "../public");
+const public = path.resolve(__dirname, "../docs");
 const rootPath = path.resolve(__dirname, "../");
 
 const bundles = {
